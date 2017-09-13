f6f316480407c2ebdda437d9fba08d300c65c1eb
Federico Tomas Rocca <frocca17@gmail.com>
2017-09-12 21:57:13 -0300

    - Agregados ejercicios y tareas de clase 6, input

---
+++ Ejercicios clase 6/Assets/Scripts/RandomForce.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomForce : MonoBehaviour {
+
+    public int Impulse;
+    public GameObject[] MyGObjects;
+
+	// Use this for initialization
+	void Start () {
+		
+        for( int i = 0; i < MyGObjects.Length; i++ )
+        {
+            MyGObjects[i].AddComponent<ConstantForce>();
+            MyGObjects[i].GetComponent<Rigidbody>().AddForce(new Vector3( Random.Range(-1,2), Random.Range(-1, 2), Random.Range(-1, 2)) * Impulse);
+        }
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Input/Assets/Scripts/KeyboardInput.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyboardInput : MonoBehaviour {
+
+    private bool IsJumping;
+    public int Force;
+    private Rigidbody MyRigidBody;
+
+    // Use this for initialization
+    void Start () {
+
+        IsJumping = false;
+        MyRigidBody = GetComponent<Rigidbody>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        CheckCharMove();
+
+        if( Input.GetKeyDown( KeyCode.Space ) )
+        {
+            CharJump();
+        }
+		
+	}
+
+    void CheckCharMove()
+    {
+
+        if(!IsJumping)
+        {
+
+            if( Input.GetKey( KeyCode.RightArrow ) )
+            {
+                MoveRight();
+            }
+            else if(Input.GetKey(KeyCode.LeftArrow))
+            {
+                MoveLeft();
+            }
+
+        }
+
+    }
+
+    void MoveRight()
+    {
+        transform.position = transform.position + (Vector3.right * Time.deltaTime * Force);
+    }
+
+    void MoveLeft()
+    {
+        transform.position = transform.position + Vector3.left * Time.deltaTime * Force;
+    }
+
+    void CharJump()
+    {
+        IsJumping = true;
+        MyRigidBody.AddForce(Vector3.up * Force);
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if( collision.gameObject.tag == "Piso" )
+        {
+            IsJumping = false;
+        }
+    }
+
+}
