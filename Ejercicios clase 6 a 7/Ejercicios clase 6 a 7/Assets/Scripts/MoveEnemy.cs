a7bbb1dfd86b958e5e1b73c4edc01d15d156a50b
Federico Tomas Rocca <frocca17@gmail.com>
2017-09-13 23:09:40 -0300

    - Terminada tarea Ejercicios clase 6 	- Agregado Ejercicios de clase 6 a 7

---
+++ Ejercicios clase 6 a 7/Ejercicios clase 6 a 7/Assets/Scripts/MoveEnemy.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveEnemy : MonoBehaviour {
+
+    public int Step = 10;
+    public Vector3 InitialPosition = new Vector3(0, 0, 10);
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = transform.position + (Vector3.back * Time.deltaTime * Step);
+
+        if(transform.position.z <= -10 )
+        {
+            transform.position = InitialPosition;
+            transform.position = new Vector3( Random.Range(-5,6), Random.Range(-5, 6), InitialPosition.z ) ;
+        }
+
+	}
+}
