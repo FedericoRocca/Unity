8d28d0ab688ccf38c3d20ea18bea745070145a6e
Federico Tomas Rocca <frocca17@gmail.com>
2017-09-16 16:25:44 -0300

    - Agregado el color random al back de las cartas, ejercicio 	memotest clase 7

--- Clase 7/Assets/Scripts/MemoTest/AsignRandomColor.cs
+++ Clase 7/Assets/Scripts/MemoTest/AsignRandomColor.cs
@@ -9,11 +9,28 @@ public class AsignRandomColor : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        IsAssigned = new bool[Cards.Length];
 
-        for( int i = 0; i < Cards.Length; i++ )
+        IsAssigned = new bool[Cards.Length];
+        for( int r = 0; r < IsAssigned.Length; r++ ) // Inicializo en false
         {
-            
+            IsAssigned[r] = false;
+        }
+
+        for( int i = 0; i < Cards.Length; i++ ) // Recorro las cartas
+        {
+
+            Cards[i].GetComponent<Renderer>().material.color = Random.ColorHSV(); // Asigno un color aleatorio a la carta
+            for(int a = 0; a < Cards.Length; a++) // Como cada carta tiene que tener su par, asigno el color de la carta a otra en la grilla
+            {
+
+                if( IsAssigned[a] == false ) // Si no fue asignado el color, busco otra
+                {
+                    Cards[a].GetComponent<Renderer>().material.color = Cards[i].GetComponent<Renderer>().material.color;
+                    IsAssigned[a] = true;
+                }
+
+            }
+
         }
 
 	}
