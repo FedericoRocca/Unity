5d4804ca9dc6af8cb9adeb6b1890be99d9f2776e
Federico Tomas Rocca <frocca17@gmail.com>
2017-09-19 21:50:51 -0300

    - Agregados ejercicios de prefabs, clase 8 (Sin terminar)

---
+++ Clase 8/Assets/Scripts/MoveGrua.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveGrua : MonoBehaviour {
+
+	private bool MovingLeft = true;
+    public float Speed = 0;
+    public GameObject CubePrefab;
+
+	void Start () {
+		
+	}
+
+	void Update () {
+
+		if (MovingLeft == true)
+        {
+            transform.position = transform.position + ( Vector3.left * Time.deltaTime * Speed );
+        }
+        else
+        {
+            transform.position = transform.position + (Vector3.right * Time.deltaTime * Speed);
+        }
+
+        if(transform.position.x >= 5)
+        {
+            MovingLeft = true;
+        }
+        else if(transform.position.x <= -5)
+        {
+            MovingLeft = false;
+        }
+
+        if( true )
+        {
+
+        }
+
+	}
+}
--- Ejercicios clase 6 a 7/Ejercicios clase 6 a 7/Assets/Scripts/MoveMarioPlayer.cs
+++ Ejercicios clase 6 a 7/Ejercicios clase 6 a 7/Assets/Scripts/MoveMarioPlayer.cs
@@ -5,19 +5,32 @@ using UnityEngine;
 public class MoveMarioPlayer : MonoBehaviour {
 
     public int Step = 5;
+    private Rigidbody MyRigidBody;
 
 	// Use this for initialization
 	void Start () {
-		
+
+        MyRigidBody = GetComponent<Rigidbody>();
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
-        if( Input.GetKey( KeyCode.RightArrow ) && transform.position.x >= 7 )
+        if( Input.GetKey( KeyCode.RightArrow ) && transform.position.x <= 7 )
         {
             transform.position = transform.position + (Vector3.right * Time.deltaTime * Step);
         }
 
-	}
+        if (Input.GetKey(KeyCode.LeftArrow) && transform.position.x >= -7)
+        {
+            transform.position = transform.position + (Vector3.left * Time.deltaTime * Step);
+        }
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            MyRigidBody.AddForce(Vector3.up * 200);
+        }
+
+    }
 }
