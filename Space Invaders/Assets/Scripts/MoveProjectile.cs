04ecf8282c62b6b92bebef80aced2d70e04c0592
Federico Tomas Rocca <frocca17@gmail.com>
2017-09-29 21:57:22 -0300

    -Empezado el space invaders

---
+++ Space Invaders/Assets/Scripts/MoveProjectile.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveProjectile : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void FixedUpdate()
+	{
+		gameObject.GetComponent<Rigidbody>().AddForce(Vector3.down * 10, ForceMode.Force);
+	}
+
+}
---
+++ Space Invaders/Assets/Scripts/Shoot.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour {
+
+	public GameObject EnemyProjectile;
+	public GameObject WeaponPos;
+	private const float ShootTime = 2.0f;
+	private float ShootAt;
+
+	// Use this for initialization
+	void Start () {
+		ShootAt = ShootTime;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		ShootAt -= Time.deltaTime;
+
+		if( ShootAt <= 0.0f )
+		{
+			Instantiate( EnemyProjectile, WeaponPos.transform.position, Quaternion.identity );
+			ShootAt = ShootTime;
+		}
+
+	}
+}
