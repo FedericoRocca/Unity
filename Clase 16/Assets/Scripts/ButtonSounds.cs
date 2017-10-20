3f37edcbc50bbd8e04e1e944d5909fb14b88df90
Federico Tomas Rocca <frocca17@gmail.com>
2017-10-20 18:28:10 -0300

    - Agregada clase 16 		- Manejo de sonidos
    	- Fat Runner
    		- Creado proyecto
    		- Agregado GDD
    
    	- The Last One
    		- Creado proyecto
    		- Agregado GDD

---
+++ Clase 16/Assets/Scripts/ButtonSounds.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonSounds : MonoBehaviour {
+
+	public Button PlaySoundButton;
+
+	public AudioClip SoundClip;
+
+	public AudioSource AudioSourcePlaySound;
+
+	// Use this for initialization
+	void Start () {
+		PlaySoundButton.onClick.AddListener(PlayTheSound);
+	}
+	
+	// Update is called once per frame
+	void PlayTheSound () 
+	{
+		AudioSourcePlaySound.clip = SoundClip;
+		AudioSourcePlaySound.Play();
+	}
+}
---
+++ Clase 16/Assets/Scripts/MusicManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MusicManager : MonoBehaviour {
+
+	public Button PlayStopMusicButton;
+	
+	public AudioClip MusicClip;
+
+	public AudioSource MusicSource;
+
+	public Slider VolumeAndPitch;
+
+	// Use this for initialization
+	void Start () {
+		VolumeAndPitch.value = 0.5f;
+		VolumeAndPitch.onValueChanged.AddListener(SetVolumeAndPitch);
+		MusicSource.volume = VolumeAndPitch.value;
+		PlayStopMusicButton.onClick.AddListener(ToggleMusicPlay);
+		MusicSource.clip = MusicClip;
+		MusicSource.loop = true;
+		MusicSource.Play();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void ToggleMusicPlay()
+	{
+		if( MusicSource.isPlaying )
+		{
+			MusicSource.Stop();
+		}
+		else
+		{
+			MusicSource.Play();
+		}
+	}
+
+	void SetVolumeAndPitch(float NewValue)
+	{
+		MusicSource.volume = NewValue;
+		MusicSource.pitch = NewValue;
+	}
+}
