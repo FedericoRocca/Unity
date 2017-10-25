35ea8f325e75f45497217f16a81c2ee3d647e7de
Federico Tomas Rocca <frocca17@gmail.com>
2017-10-24 21:54:04 -0300

    - Se elimina proyecto 2D creado en 3D 	- Se agrega Clase 18, animaciones 	- Fat Runner, se crea nuevamente proyecto en 2D 		- Escena de pruebas 		- Script de movimiento de jugador terminado

---
+++ Fat Runner/Assets/Scripts/Player/Movement/PlayerMove.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour {
+	
+	//Variables para uso externo
+	public int Step;
+	public int JumpForce;
+	public int EnemyJumpForce;
+
+	// Variables para uso del script
+	private Rigidbody2D MyRigidBody;
+	private bool IsInAir;
+
+	// Use this for initialization
+	void Start () {
+		MyRigidBody = transform.GetComponent<Rigidbody2D>();
+		IsInAir = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		// Movimiento izquierda
+		if( Input.GetKey( KeyCode.A ) || Input.GetKey( KeyCode.LeftArrow ) )
+		{
+			MoveLeft();
+		}
+
+		// Movimiento derecha
+		if( Input.GetKey( KeyCode.D ) || Input.GetKey( KeyCode.RightArrow ) )
+		{
+			MoveRight();
+		}
+
+		// Salto
+		if( Input.GetKeyDown( KeyCode.Space ) || Input.GetKey( KeyCode.UpArrow ) )
+		{
+			Jump();
+		}
+
+	}
+
+	private void MoveLeft()
+	{
+		transform.Translate( Vector3.left * Step * Time.deltaTime );
+	}
+
+	private void MoveRight()
+	{
+        transform.Translate( Vector3.right * Step * Time.deltaTime );
+	}
+
+	private void Jump()
+	{
+        if( !IsInAir )
+		{
+			MyRigidBody.AddForce(Vector3.up * JumpForce, ForceMode2D.Force);
+			IsInAir = true;
+		}
+	}
+
+	private void BounceOnEnemy()
+	{
+		MyRigidBody.AddForce(Vector3.up * EnemyJumpForce, ForceMode2D.Force);
+	}
+
+	void OnCollisionEnter2D(Collision2D other)
+	{
+
+		if( other.transform.tag.Equals("Floor") )
+		{
+			IsInAir = false;
+		}
+
+		if( other.transform.tag.Equals("Enemy") )
+		{
+			if( other.contacts[0].point.y == other.contacts[1].point.y )
+			{
+				BounceOnEnemy();
+			}
+			else
+			{
+				Destroy( this.gameObject );
+			}
+		}
+
+	}
+
+}
