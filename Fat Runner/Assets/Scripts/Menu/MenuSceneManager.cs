de8fd3e4691ce4700edde60442b78154ebe4d03c
Federico Tomas Rocca <frocca17@gmail.com>
2017-11-14 21:03:35 -0300

    - Fat Runner

--- Fat Runner/Assets/Scripts/Menu/MenuSceneManager.cs
+++ Fat Runner/Assets/Scripts/Menu/MenuSceneManager.cs
@@ -10,14 +10,24 @@ public class MenuSceneManager : MonoBehaviour {
         SceneManager.LoadScene("InGame", LoadSceneMode.Single);
     }
 
+    public void GoToCredits()
+    {
+        SceneManager.LoadScene("Credits", LoadSceneMode.Single);
+    }
+
     public void ExitGame()
     {
         Application.Quit();
     }
 
-    public void CreditsScene()
+    public void GoToMenu()
     {
-        SceneManager.LoadScene("Credits", LoadSceneMode.Additive);
+        SceneManager.LoadScene("MenuPrincipal", LoadSceneMode.Single);
+    }
+
+    public void GoToGameOver()
+    {
+        SceneManager.LoadScene("GameOver", LoadSceneMode.Single);
     }
 
 }
