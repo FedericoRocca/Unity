4e9788da803f4c033fff14cb0eed546694c3dd3f
Federico Tomas Rocca <frocca17@gmail.com>
2017-11-22 00:34:51 -0300

    - The Last One - Se empieza a trabajar sobre proyecto 3D. Se agrega personaje y scripts de movimiento

---
+++ The Last One/Assets/Scripts/Player/Movement/WeapontMovement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeapontMovement : MonoBehaviour {
+
+    private Transform MyTransform;
+
+	// Use this for initialization
+	void Start () {
+        MyTransform = GetComponentInParent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.Log("Rotation: ", MyTransform);
+        MyTransform.rotation = Camera.main.transform.rotation;
+	}
+}
