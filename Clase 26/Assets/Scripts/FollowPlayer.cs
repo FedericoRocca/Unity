285de62cd0b1457d1c1d54115b1ee759b3e21cdd
Federico Tomas Rocca <frocca17@gmail.com>
2017-11-25 14:09:48 -0300

    Subida de cambios en The Last One

---
+++ Clase 26/Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class FollowPlayer : MonoBehaviour {
+
+    public Transform PlayerPos;
+    public NavMeshAgent MyNavMesh;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        MyNavMesh.SetDestination(PlayerPos.position);
+	}
+}
