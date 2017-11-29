29be856f5c75ad0c6b8d742180503bdfc4af1fe5
Federico Tomas Rocca <frocca17@gmail.com>
2017-11-28 23:59:21 -0300

    - The Last One - Se agrega primera implementacion de recoil de arma (A Corregir) - Se elimina Grass para mejorar la performance dentro del editor

---
+++ The Last One/Assets/Scripts/Player/Shoot.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour {
+
+    private bool TryShoot = false;
+    public float Cadency;
+    private float TimePassed = 0.0f;
+    private bool IsShooting = false;
+    public GameObject Weapon;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if( Input.GetMouseButton( 0 ) )
+        {
+            TryShoot = true;
+        }
+
+        if( Input.GetMouseButtonUp( 0 ) )
+        {
+            TryShoot = false;
+        }
+
+        TimePassed += Time.deltaTime;
+
+        if( TimePassed >= 0.5f )
+        {
+            TimePassed = 0.0f;
+            IsShooting = false;
+        }
+        else
+        {
+            IsShooting = true;
+        }
+
+        if( IsShooting )
+        {
+            Weapon.transform.Rotate(Weapon.transform.rotation.x - 7 * Time.deltaTime, Weapon.transform.rotation.y, Weapon.transform.rotation.z);
+        }
+        else
+        {
+            Weapon.transform.Rotate(Weapon.transform.rotation.x * Time.deltaTime, Weapon.transform.rotation.y, Weapon.transform.rotation.z);
+        }
+	
+	}
+}
